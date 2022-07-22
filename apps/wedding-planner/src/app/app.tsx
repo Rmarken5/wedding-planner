5a0053bc97be8921a30c519490ab79cb53ef71ee
Ryan Marken <marken.ryan@gmail.com>
2022-07-21 20:51:04 -0400

    Auth stuff

--- apps/wedding-planner/src/app/app.tsx
+++ apps/wedding-planner/src/app/app.tsx
@@ -5,7 +5,6 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
-import { SignInPageSignIn } from '@marken-shop-react/sign-in/page/sign-in';
 import { NotFoundPageNotFound } from '@marken-shop-react/not-found/page/not-found';
 import React from 'react';
 import { ThemePageTheme } from '@marken-shop-react/theme/page/theme';
@@ -13,6 +12,7 @@ import { ProtectedRouteSharedProps } from '@marken-shop-react/protected-route/sh
 import { initializeApp } from 'firebase/app';
 import { environment } from '../environments/environment';
 import { getAuth, signInWithEmailAndPassword, User } from 'firebase/auth';
+import { SignInPageSignIn } from '@marken-shop-react/sign-in/page/sign-in';
 
 interface AppState {
   user?: User;
@@ -21,36 +21,42 @@ interface AppState {
 
 const ProtectedRouteShared = ({
   isAuthenticated,
-  redirectPath = '/sign-in',
+  redirectPath = '/',
   children,
 }: ProtectedRouteSharedProps) => {
   if (!isAuthenticated) {
     return <Navigate to={redirectPath} replace />;
   }
-
   return children ? children : <Outlet />;
 };
 
 export class App extends React.Component<any, AppState> {
   constructor(props: any) {
+    const { firebaseConfig } = environment;
     console.log(environment);
     super(props);
     this.state = {};
-    const { firebaseConfig } = environment;
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      this.state = {
+        ...this.state,
+        user: JSON.parse(userString),
+      };
+    }
 
     const app = initializeApp(firebaseConfig);
   }
 
-  private async loginClick(email: string, password: string) {
+  private async loginClick(email: string, password: string): Promise<void> {
     const auth = getAuth();
     try {
       const response = await signInWithEmailAndPassword(auth, email, password);
-      response.user;
       this.setState({
         ...this.state,
         user: response.user,
         signInError: null,
       });
+      localStorage.setItem('user', JSON.stringify(response.user));
     } catch (error: any) {
       console.log('error: ', error);
       this.setState({
@@ -63,8 +69,41 @@ export class App extends React.Component<any, AppState> {
     console.log(this.state);
   }
 
+  private async logout(): Promise<void> {
+    this.setState({
+      ...this.state,
+      user: undefined,
+    });
+    localStorage.removeItem('user');
+  }
+
   override render() {
     console.log(this.state);
+    let home;
+    if (!this.state?.user) {
+      home = (
+        <Route
+          path="/"
+          element={
+            <SignInPageSignIn
+              loginClick={this.loginClick.bind(this)}
+              signInLink={'/theme'}
+              signInError={this.state.signInError}
+            />
+          }
+        />
+      );
+    } else {
+      home = (
+        <Route
+          element={
+            <ProtectedRouteShared isAuthenticated={!!this.state?.user} />
+          }
+        >
+          <Route path="/" element={<ThemePageTheme />} />
+        </Route>
+      );
+    }
     return (
       <div className={'h-screen bg-cyan-100'}>
         <BrowserRouter>
@@ -76,16 +115,7 @@ export class App extends React.Component<any, AppState> {
             >
               <Route path="/theme" element={<ThemePageTheme />} />
             </Route>
-            <Route
-              path="/sign-in"
-              element={
-                <SignInPageSignIn
-                  loginClick={this.loginClick.bind(this)}
-                  signInLink={'/theme'}
-                  signInError={this.state.signInError}
-                />
-              }
-            />
+            {home}
             <Route path="*" element={<NotFoundPageNotFound />} />
           </Routes>
         </BrowserRouter>
